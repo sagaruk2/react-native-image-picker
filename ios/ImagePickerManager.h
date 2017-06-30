44b849a58950790dd27364858a943d8d1c6dfd82
sagaruk2 <ukirade.sagar@gmail.com>
2017-06-30 12:53:44 +0530

    modify import Statment

--- ios/ImagePickerManager.h
+++ ios/ImagePickerManager.h
@@ -1,5 +1,9 @@
-#import <React/RCTBridgeModule.h>
 #import <UIKit/UIKit.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
 typedef NS_ENUM(NSInteger, RNImagePickerTarget) {
   RNImagePickerTargetCamera = 1,
